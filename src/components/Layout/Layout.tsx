5ce2441131ae0699485ef82ea7dd931065bc920f
Gleb <podiumfffuuu@gmail.com>
2023-05-01 17:18:33 +0200

    fix: remove test height value

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,7 +4,7 @@ import { Fragment, PropsWithChildren } from "react";
 
 export const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <div className="flex min-h-[100vh] flex-col">
+    <div className="flex flex-col">
       <Header />
       <main className="flex h-[1000px] w-full grow flex-col items-center p-1 pt-16">
         {children}
