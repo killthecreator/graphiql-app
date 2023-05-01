5cd8bd704c18b972f0c38758eaa3cd4c183ebc7d
Alex <alexmihailov91@gmail.com>
2023-05-01 20:14:58 +0500

    feat: edit skeleton layout

--- src/components/ui/graphqlResponseSkeleton.tsx
+++ src/components/ui/graphqlResponseSkeleton.tsx
@@ -5,16 +5,21 @@ const GraphqlResponseSkeleton = () => (
   <ContentLoader
     speed={2}
     width={400}
-    height={160}
-    viewBox="0 0 400 160"
+    height={320}
+    viewBox="0 0 400 320"
     backgroundColor="#f3f3f3"
     foregroundColor="#ecebeb"
   >
     <rect x="20" y="2" rx="3" ry="3" width="88" height="19" />
-    <rect x="20" y="58" rx="3" ry="3" width="324" height="20" />
+    <rect x="20" y="58" rx="3" ry="3" width="247" height="20" />
     <rect x="20" y="87" rx="3" ry="3" width="178" height="20" />
     <rect x="20" y="116" rx="3" ry="3" width="153" height="20" />
     <rect x="20" y="30" rx="3" ry="3" width="171" height="20" />
+    <rect x="20" y="160" rx="3" ry="3" width="88" height="19" />
+    <rect x="20" y="216" rx="3" ry="3" width="288" height="20" />
+    <rect x="20" y="245" rx="3" ry="3" width="178" height="20" />
+    <rect x="20" y="274" rx="3" ry="3" width="153" height="20" />
+    <rect x="20" y="188" rx="3" ry="3" width="171" height="20" />
   </ContentLoader>
 );
 
