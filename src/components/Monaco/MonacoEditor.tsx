86de229a353bf960fd0f3672995d4c9679385860
Gleb <podiumfffuuu@gmail.com>
2023-05-15 16:01:57 +0200

    feat: add more skeletons on editor

--- src/components/Monaco/MonacoEditor.tsx
+++ src/components/Monaco/MonacoEditor.tsx
@@ -1,11 +1,31 @@
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 import { Skeleton } from "~/components/ui/";
 
-export const MonacoEditor = dynamic(() => import('~/monaco').then(module => module.MonacoEditor), {
-  ssr: false,
-  loading: () => (<div className="space-y-2">
+export const MonacoEditor = dynamic(
+  () => import("~/monaco").then((module) => module.MonacoEditor),
+  {
+    ssr: false,
+    loading: () => (
+      <div className="space-y-2">
         <Skeleton className="h-4 w-[220px]" />
         <Skeleton className="h-4 w-[250px]" />
         <Skeleton className="h-4 w-[180px]" />
-    </div>),
-});
+        <Skeleton className="h-4 w-[220px]" />
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[180px]" />
+        <Skeleton className="h-4 w-[220px]" />
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[180px]" />
+        <Skeleton className="h-4 w-[220px]" />
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[180px]" />
+        <Skeleton className="h-4 w-[220px]" />
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[180px]" />
+        <Skeleton className="h-4 w-[220px]" />
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[180px]" />
+      </div>
+    ),
+  }
+);
