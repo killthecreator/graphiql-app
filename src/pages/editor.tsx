e0f04f37fda3bf45cd484ad986e3a031e54fc84a
Gleb <podiumfffuuu@gmail.com>
2023-05-15 14:18:23 +0200

    feat: disable ssr for editor page loading

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,6 +1,7 @@
 import { type NextPage } from "next";
 
 import Head from "next/head";
+import dynamic from "next/dynamic";
 
 import {
   Accordion,
@@ -330,4 +331,6 @@ const Editor: NextPage = () => {
   );
 };
 
-export default Editor;
+export default dynamic(() => Promise.resolve(Editor), {
+  ssr: false,
+});
