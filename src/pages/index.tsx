28652c9702902348398259a0135c38cbb68cf78c
Gleb <podiumfffuuu@gmail.com>
2023-05-17 15:32:03 +0200

    fix: fix image height

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,7 +27,7 @@ import { profileCards, dataCards } from "~/data";
 const articleClass =
   "grid gap-[5vw] grid-cols-1 even:sm:grid-cols-[1fr_50%] odd:sm:grid-cols-[50%_1fr]";
 const imageClass =
-  "rounded-lg w-full h-full object-contain h-fit self-center order-first sm:order-none";
+  "rounded-lg w-full object-contain h-fit self-center order-first sm:order-none";
 
 const introspectiveExample = `
 {
